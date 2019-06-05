5e8b1570a7b1ed49a5870a8bff726af00322b342
Darrk Moberg <30670444+mdarrik@users.noreply.github.com>
2019-06-04 21:12:43 -0700

    Feature: Adds Info to the Home Page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,16 @@
 import React from "react"
 
-export default () => <div>Hello world!</div>
+export default () => {
+  return (
+    <main>
+      <h1> Static Site Comments/Forum </h1>
+      <section>
+      <h2>About</h2>
+      <p>
+        This site is a simple idea for what a basic, static forum site/comments section could look like using Gatsby + Netlify.
+        Uses Azure Webhooks to trigger new Gatsby builds when comments are submitted.
+      </p>
+      </section>
+    </main>
+  )
+}
