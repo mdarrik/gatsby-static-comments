1c5b78ddb372bc067285e9790be838c0d8166ddf
Darrk Moberg <30670444+mdarrik@users.noreply.github.com>
2019-06-04 21:24:32 -0700

    Refactor: Refactors NewCommentBox to use context

--- src/components/new-comment-box.jsx
+++ src/components/new-comment-box.jsx
@@ -1,22 +1,33 @@
-import React from "react"
+import React, { useContext } from 'react'
 
-const CommentBox = () => {
-    const submitComment = (event) => {
-        event.preventDefault()
-        console.log(event.target.querySelector('textarea').value)
+import CommentsContext from '../context/comments-context'
+
+const NewCommentBox = () => {
+  const { addCommentToComments } = useContext(CommentsContext)
+  const submitComment = event => {
+    event.preventDefault()
+    const commentTextArea = event.target.querySelector("textarea")
+    const newComment = {
+      commentText: commentTextArea.value,
+      commentDate: new Date(),
+      author: "an author",
     }
 
-    return (
-        <form className="comment-form" onSubmit={submitComment}>
-            <label>
-                Add a new comment
+    addCommentToComments(newComment)
+    commentTextArea.value = ""
+  }
+
+  return (
+    <form className="comment-form" onSubmit={submitComment}>
+      <label>
+        Add a new comment
         <textarea className="comment-input" />
-            </label>
+      </label>
       <div className="comment-footer">
-                <button type="submit">Add Comment</button>
-            </div>
-        </form>
-    )
+        <button type="submit">Add Comment</button>
+      </div>
+    </form>
+  )
 }
 
-export default CommentBox
+export default NewCommentBox
