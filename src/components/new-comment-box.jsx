ff45f4d70c27fe3aa4f67429cb97ccfbdb497bae
Darrk Moberg <30670444+mdarrik@users.noreply.github.com>
2019-06-15 20:02:03 -0700

    feature: Integrates the comment form with netlify forms

--- src/components/new-comment-box.jsx
+++ src/components/new-comment-box.jsx
@@ -1,27 +1,49 @@
-import React, { useContext } from 'react'
+import React, { useContext } from "react"
 
-import CommentsContext from '../context/comments-context'
+import CommentsContext from "../context/comments-context"
 
 const NewCommentBox = () => {
   const { addCommentToComments } = useContext(CommentsContext)
-  const submitComment = event => {
+  const submitComment = (event) => {
     event.preventDefault()
-    const commentTextArea = event.target.querySelector("textarea")
-    const newComment = {
-      commentText: commentTextArea.value,
-      commentDate: new Date(),
-      author: "an author",
+    const {
+      author: authorField,
+      "form-name": formNameField,
+      "comment-text": commentTextField,
+    } = event.target.elements
+
+    const formData = {
+      author: authorField.value,
+      "form-name": formNameField.value,
+      "comment-text": commentTextField.value,
     }
 
-    addCommentToComments(newComment)
-    commentTextArea.value = ""
+    fetch("/", {
+      method: "POST",
+      headers: {"Content-Type": "application/x-www-form-urlencoded"},
+      body: Object.keys(formData)
+      .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(formData[key]))
+      .join("&")
+    }).then((resp) => console.log(resp)).catch((error) => console.log(error))
+
+    addCommentToComments({commentText: commentTextField.value, author: authorField.value, commentDate: new Date()})
   }
 
   return (
-    <form className="comment-form" onSubmit={submitComment}>
+    <form
+      name="comment"
+      className="comment-form"
+      onSubmit={submitComment}
+      data-netlify="true"
+    >
+      <input type="hidden" value="comment" name="form-name" />
       <label>
         Add a new comment
-        <textarea className="comment-input" />
+        <textarea className="comment-input" name="comment-text" />
+      </label>
+      <label>
+        Author:
+        <input type="text" readOnly value="An author" name="author" />
       </label>
       <div className="comment-footer">
         <button type="submit">Add Comment</button>
